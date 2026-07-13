0addd679704d1294c4a76f6091387c3ec66dbbd4
jfetgarcia <jfetgarcia@gmail.com>
2026-07-12 22:23:15 -0600

    Add CPU and RAM to system config
    General layout of the system and added
    connections for devices

--- gem5/conf/tlm_gem5_cpu_master.py
+++ gem5/conf/tlm_gem5_cpu_master.py
@@ -9,6 +9,34 @@ system.clk_domain = SrcClockDomain(
     voltage_domain=VoltageDomain("1V")
 )
 
+system.cpu = ArmTimingSimpleCPU()
+
+# Bus
+system.membus = IOXBar(width=16)
+
+# RAM memory
+system.mem_ctrl = SimpleMemory()
+
+# TODO: Check if init addr needs to be specified
+system.mem_ctrl.range = AddrRange(size="64MiB")
+
+# Bridge to SystemC module
+system.tlm = ExternalSlave()
+# TODO: Update address ranges
+system.tlm.addr_ranges = [AddrRange("64MiB")]
+system.tlm.port_type = "tlm_slave"
+system.tlm.port_data = "transactor"
+
+# TODO: Set binary example to load on CPU
+
+# Connections
+system.cpu.icache_port = system.membus.cpu_side_ports
+system.cpu.icache_port = system.membus.cpu_side_ports
+
+system.mem_ctrl.port = system.membus.mem_side_ports
+
+system.membus.mem_side_ports = system.tlm.port
+
 # Simulation start
 root = Root(full_system=False, system=system)
 root.system.mem_mode = "timing"
