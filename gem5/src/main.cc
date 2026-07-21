611d1480ce148e1571f5877355b837aef193ae05
jfetgarcia <jfetgarcia@gmail.com>
2026-07-21 15:37:01 -0600

    Base Main script to parse simulation configs

---
+++ gem5/src/main.cc
@@ -0,0 +1,16 @@
+
+
+#include <systemc>
+#include <tlm>
+
+#include "cli_parser.hh"
+#include "report_handler.hh"
+#include "accelerator.hh"
+#include "sim_control.hh"
+#include "slave_transactor.hh"
+#include "stats.hh"
+
+int sc_main (int argc, char **argv)
+{
+
+}
